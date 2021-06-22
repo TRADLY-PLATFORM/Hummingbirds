d76fade7fc9a3c2e2210a64620793706a8e75316
unknown <siddiq@digivalsolutions.comgit config --global user.name siddiq-digival>
2021-06-22 09:04:04 +0530

    sitemap generator added

---
+++ config.js
@@ -0,0 +1,42 @@
+/*
+ * *
+ *  Copyright 2014 Comcast Cable Communications Management, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+
+ /*
+ * ** IMPORTANT ** RENAME THIS FILE TO 'config.js' BEFORE RUNNING THE SCRIPT
+ */
+
+module.exports  = {
+    base: 'https://api.tradly.app', // base url of the website
+    urls: [
+        'https://api.tradly.app' // URLs to scan (works only if autoCrawl is false)
+    ],
+    strictPresence: 'https://api.tradly.app/', // Crawl links only if this is present
+    ignoreStrings: [ // Ignore any link with this string
+        'img.xfinity',
+        'styles.',
+        'm.me'
+    ],
+    autoCrawl: false, // Automatically crawl through internal links and create a sitemap
+    crawlLevel: 1,
+    pageLoad: {
+        waitUntil: 'networkidle0', // puppeteer config
+        timeout: 3000000
+    },
+    disableHashRoutes: true,
+    sortBy: 'asc' // 'asc' | 'dsc' | 'none'
+}
