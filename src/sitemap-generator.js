1cf7b28008fc73b162672477e229ed5900cf1e12
JK Baseer <61427976+jk-baseer@users.noreply.github.com>
2021-07-15 20:50:48 +0530

    Update sitemap-generator.js

--- src/sitemap-generator.js
+++ src/sitemap-generator.js
@@ -6,7 +6,7 @@ const router = require('./routes').default;
 const Sitemap = require('react-router-sitemap').default;
 
 function generateSitemap() {
-  return new Sitemap(router).build('http://localhost:3000').save('./public/sitemap.xml');
+  return new Sitemap(router).build('https://web.nammakada.app').save('./public/sitemap.xml');
 }
 
 generateSitemap();
